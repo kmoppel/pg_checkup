44fd6ec57e8d3db7ab7e94a006ba128118565e5b
Kaarel Moppel <kaarel.moppel@gmail.com>
2021-04-02 17:39:10 +0300

    server_non_default_params.sql better SQL

--- server_non_default_params.sql
+++ server_non_default_params.sql
@@ -1,15 +1,19 @@
 SELECT
   name,
-  category,
-  case when length(current_setting(name)) < 40 then current_setting(name) else current_setting(name)::varchar(37) || '...' end,
+  -- category,
+  case when length(current_setting(name)) < 50 then current_setting(name) else current_setting(name)::varchar(47) || '...' end as current_setting,
+  reset_val,
   boot_val,
   unit,
   source
 FROM
   pg_settings
-WHERE source NOT IN ('default', 'override') --compiled or cmdline
-AND NOT category ~* 'client'
+WHERE NOT category ~* 'formatting'
 AND NOT category ~* 'logging'
-AND NOT category ~* 'authent'
+AND NOT category ~* 'ssl'
+AND NOT name ~ 'file'
+AND NOT name ~ 'directory'
+AND NOT name IN ('cluster_name', 'max_stack_depth')
+AND boot_val IS DISTINCT FROM reset_val
 ORDER BY
-  category, name;
+  name;
