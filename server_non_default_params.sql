9b65a0c12e0d83b16ce1bc9aa3015ccd7aecfc37
Kaarel Moppel <kaarel.moppel@gmail.com>
2021-05-24 15:20:11 +0300

    server_non_default_params - add back category + shorten also reset_val

--- server_non_default_params.sql
+++ server_non_default_params.sql
@@ -1,8 +1,8 @@
 SELECT
+  (regexp_split_to_array(category, ' '))[1] as category_shortened,
   name,
-  -- category,
   case when length(current_setting(name)) < 50 then current_setting(name) else current_setting(name)::varchar(47) || '...' end as current_setting,
-  reset_val,
+  case when length(reset_val) < 50 then reset_val else reset_val::varchar(47) || '...' end as reset_val,
   boot_val,
   unit,
   source
@@ -16,4 +16,5 @@ AND NOT name ~ 'directory'
 AND NOT name IN ('cluster_name', 'max_stack_depth')
 AND boot_val IS DISTINCT FROM reset_val
 ORDER BY
+  category,
   name;
