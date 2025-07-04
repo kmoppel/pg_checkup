abd2df6896f2e5f5939540b6b0fce9cf626dd4b5
Kaarel Moppel <kaarel.moppel@gmail.com>
2025-07-04 12:06:57 +0300

    Add stat_statements_snapshotting_simple.sql
    For ad-hoc PGSS monitoring on the DB server when no monitoring tools exists

---
+++ stat_statements_snapshotting_simple.sql
@@ -0,0 +1,101 @@
+create unlogged table pg_stat_statements_snapshots as
+	select now(), dbid, queryid, calls, total_exec_time, mean_exec_time, stddev_exec_time, rows, blk_read_time, blk_write_time, wal_bytes, ltrim(regexp_replace(query, E'[ \\t\\n\\r]+' , ' ', 'g'))::varchar(200) as query
+	from pg_stat_statements where false ;
+
+
+
+insert into pg_stat_statements_snapshots
+  select now(), dbid, queryid, calls, total_exec_time, mean_exec_time, stddev_exec_time, rows,
+    blk_read_time, blk_write_time, wal_bytes, ltrim(regexp_replace(query, E'[ \\t\\n\\r]+' , ' ', 'g'))::varchar(200) as query
+  from pg_stat_statements;
+
+-- \watch 300
+
+
+-- mean_exec_time, QPS
+with q as (
+  select
+    now,
+    sum(rows) rows,
+    sum(calls) calls,
+    avg(mean_exec_time) mean_exec_time
+  from
+    pg_stat_statements_snapshots
+  group by
+    now
+), qt as (
+  select
+    min(now) as t1,
+    max(now) as t2,
+    extract(epoch from max(now) - min(now)) as duration_s
+  from q
+)
+select
+  qt.*,
+  (((select rows from q where now = qt.t2) - (select rows from q where now = qt.t1)) / qt.duration_s)::numeric(12,2) as avg_rows_s,
+  (((select calls from q where now = qt.t2) - (select calls from q where now = qt.t1)) / qt.duration_s)::numeric(12,2) as avg_calls_s,
+  (select avg(mean_exec_time) from q)::numeric(12,2) as avg_mean_exec_time_ms
+from qt ;
+
+
+-- top stmt
+
+-- to speed up analyzes:
+-- create index on pg_stat_statements_snapshots (queryid, now );
+
+with qt as (
+	select
+	 max(now) as t2,
+	 sum(total_exec_time) as t2_grand_total_exec_time_ms
+	from
+	  pg_stat_statements_snapshots
+)
+select
+  (100::numeric * total_exec_time / t2_grand_total_exec_time_ms)::numeric(4,2) || ' %' as tot_exec_time,
+  (total_exec_time / 1000)::int as exec_time_s,
+  (calls / 1000)::numeric(9,1) as calls_1k,
+  mean_exec_time::numeric(9,1) as mean_time_ms,
+  date_trunc('minute', "to" - "from") as stats_window,
+  queryid,
+  query
+from (
+
+	select
+	  queryid,
+	  min("from") as "from",
+	  max("to") as "to",
+	  sum(total_exec_time) as total_exec_time,
+	  sum(calls) as calls,
+	  sum(total_exec_time) / sum(calls) as mean_exec_time,
+	  max(query) as query
+	from (
+
+		select
+		  t2.queryid,
+		  t2.total_exec_time - t1.total_exec_time as total_exec_time,
+		  t2.calls - t1.calls as calls,
+		  t1.now as "from",
+		  t2.now as "to",
+		  t2.query
+		from
+		  pg_stat_statements_snapshots as t2
+		join lateral (
+		  select * from pg_stat_statements_snapshots t1
+		  where t1.queryid = t2.queryid
+		  and t2.calls > t1.calls
+		  order by now
+		  limit 1
+		  ) t1 on true
+		join qt on true
+		where t2.now = qt.t2
+
+	) x
+	group by /* merge per user stats for a query */
+	  queryid
+
+) y
+join qt on true
+order by
+  total_exec_time desc
+limit
+  10;
